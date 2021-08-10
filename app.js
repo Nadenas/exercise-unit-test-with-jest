f7ebf2e89832314d7abb6dbdbb12e57383a403f8
Almudena De La Rosa <almudenaxico@gmail.com>
2021-08-10 11:00:42 +0000

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-const sum = (a,b) => {
+// esta es mi función que suma dos números
+/*const sum = (a,b) => {
     return a + b
 }
 
+// solo un registro en consola para nosotros.
 console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum };*/
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+
+function fromDollarToYen (dollar) {
+    let yen = (dollar/oneEuroIs["USD"])*oneEuroIs["JPY"];
+    return yen;
+}
+fromDollarToYen(3.5);
+
+function fromEuroToDollar (euro) {
+    let dollar = (euro*oneEuroIs["USD"]);
+    return dollar;
+}
+fromEuroToDollar(3.5);
+
+function fromYenToPound (yen) {
+    let pound = (yen/oneEuroIs["JPY"])*oneEuroIs["GBP"];
+    return pound;
+}
+fromYenToPound(3.5);
+
+module.exports = { fromEuroToDollar };
+module.exports = { fromDollarToYen };
+module.exports = { fromYenToPound };
